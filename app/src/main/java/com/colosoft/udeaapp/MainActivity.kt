d0296b29400606338fd4b65c6335aeff4574c443
Sebastian Ospina Garcia <sebastian47121@gmail.com>
2022-05-23 18:00:41 -0500

    Se realizaró un Form con 3 cajas de texto y un botón que concatena

--- app/src/main/java/com/colosoft/udeaapp/MainActivity.kt
+++ app/src/main/java/com/colosoft/udeaapp/MainActivity.kt
@@ -1,14 +1,30 @@
 package com.colosoft.udeaapp
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.colosoft.udeaapp.databinding.ActivityMainBinding
 
 // TODO: Falta definir algo
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
+
+    private lateinit var mainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        val view = mainBinding.root
+        setContentView(view)
+
+        mainBinding.registerButton.setOnClickListener{
+            val name = mainBinding.nameEditText.text.toString()
+            val lastName = mainBinding.lastNameEditText.text.toString()
+            val email = mainBinding.emailEditText.text.toString()
+
+            mainBinding.infoTextView.text = name+"\n"+lastName+"\n"+email
+        }
     }
 }
 
