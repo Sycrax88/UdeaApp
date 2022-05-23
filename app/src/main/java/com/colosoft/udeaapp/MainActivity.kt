5698484673d73baf934433c76e9c09541c721fa1
Sebastian Ospina Garcia <sebastian47121@gmail.com>
2022-05-23 16:10:43 -0500

    Versión inicial del proyecto del Curso de Programación de Dispositivos Móviles de la Udea

---
+++ app/src/main/java/com/colosoft/udeaapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.colosoft.udeaapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+// TODO: Falta definir algo
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
